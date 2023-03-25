86f1867a5e239bda72f7fe74366b744301bb6f83
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2023-03-25 12:45:18 -0400

    add blur

--- frontend/src/components/guessing-game/guessing-game-presentation.tsx
+++ frontend/src/components/guessing-game/guessing-game-presentation.tsx
@@ -19,9 +19,9 @@ interface Props {
  * @param props
  * @returns 
  */
-export const GuessingGamePresentation =  (props: Props) => {
+export const GuessingGamePresentation = (props: Props) => {
   const {
-    firstCard, 
+    firstCard,
     secondCard,
     onPlayerChoiceClick,
     modalVisible,
@@ -30,44 +30,46 @@ export const GuessingGamePresentation =  (props: Props) => {
     winningCard,
     playerStreak,
   } = props;
-  
-  return (<>
+
+  return (
+    <>
       <div id="main-container" className={
-          cn([
-            // "bg-gray-400",
-            "max-sm:block max-sm:pt-10",
-            "max-lg:flex lg:flex",
-          ])
-        }
+        cn([
+          // "bg-gray-400",
+          "max-sm:block max-sm:pt-10",
+          "max-lg:flex lg:flex",
+        ])
+      }
       >
-      <div className={cn([
-        // "flex justify-evenly w-full"
-        "sm:flex-row",
-        "md:flex md:justify-evenly w-full"
-      ])}>
-        <CardView cardData={firstCard} imageOnClick={onPlayerChoiceClick} />
-        {/* <div className={
-          cn([
-            "max-sm:bg-red-500 max-sm:invisible",
-            "md:w-2 md:h-screen md:bg-black",
-          ])
-        }/> */}
-        {/* <div id="divider" className={cn(["w-1/3 h-full"])}></div> */}
-        <div
-          className={cn([
-            "flex", "md:flex-col",
-            "py-4 m-4 px-4",
-            // "max-sm:px-4 max-sm:py-4 max-sm:m-4"
-          ])}
-        >
-          <div className={cn(["max-sm:pr-4"])}>Current Streak</div>
-          <div className={cn(["md:text-center"])}>{playerStreak}</div>
+        <div className={cn([
+          // "flex justify-evenly w-full"
+          modalVisible ? "blur-md" : "",
+          "sm:flex-row",
+          "md:flex md:justify-evenly w-full"
+        ])}>
+          <CardView cardData={firstCard} imageOnClick={onPlayerChoiceClick} />
+          {/* <div className={
+            cn([
+              "max-sm:bg-red-500 max-sm:invisible",
+              "md:w-2 md:h-screen md:bg-black",
+            ])
+          }/> */}
+          {/* <div id="divider" className={cn(["w-1/3 h-full"])}></div> */}
+          <div
+            className={cn([
+              "flex", "md:flex-col",
+              "py-4 m-4 px-4",
+              // "max-sm:px-4 max-sm:py-4 max-sm:m-4"
+            ])}
+          >
+            <div className={cn(["max-sm:pr-4"])}>Current Streak</div>
+            <div className={cn(["md:text-center"])}>{playerStreak}</div>
+          </div>
+          <CardView cardData={secondCard} imageOnClick={onPlayerChoiceClick} />
         </div>
-        <CardView cardData={secondCard} imageOnClick={onPlayerChoiceClick}/>
-      </div>
       </div>
 
-      <ResultsModal 
+      <ResultsModal
         id="results-modal"
         visible={modalVisible}
         onModalCloseClick={onModalCloseClick}
